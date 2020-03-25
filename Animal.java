e5aaedfcf0448605ca470104e961abd84c3a41e8
Egor Zinin <60623793+Egor985@users.noreply.github.com>
2020-03-25 23:06:17 +0700

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,12 @@
+package Lesson1.classwork;
+
+public abstract class Animal {
+
+    protected int runLimit;
+    protected double jumpLimit;
+
+
+    public abstract void run(int length);
+    public abstract void jump(int height);
+
+}
